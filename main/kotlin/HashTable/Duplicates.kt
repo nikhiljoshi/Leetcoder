dc4f436513435fd7c05547165d69a3248e2d955c
nikhiljoshi <nikhiljoshi08@gmail.com>
2021-08-16 12:23:43 +0100

    Leetcoder

---
+++ main/kotlin/HashTable/Duplicates.kt
@@ -0,0 +1,16 @@
+package HashTable
+
+
+fun main() {
+    var datearray = intArrayOf(1,2,3,1)
+
+    println(Solution().containsDuplicate(datearray))
+
+
+}
+
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        return nums.toSet().size != nums.size
+    }
+}
---
+++ main/kotlin/String/Stack.kt
@@ -0,0 +1,55 @@
+package String
+
+import java.util.*
+
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+
+Example 1:
+
+Input: s = "()"
+Output: true
+ */
+fun main() {
+//    println("Is input string valid--" + Solution().isValid("{}[]{[()]}"))
+//    println("Is input string valid--" + Solution().isValid("{}{[[[]]{"))
+//    println("Is input string valid--" + Solution().isValid("{}[]{[[)]}"))
+    println("Is input string valid--" + Solution().isValid("{]"))
+    println("Is input string valid--" + Solution().isValid("{}"))
+}
+
+
+class Solution {
+    fun isValid(s: String): Boolean {
+
+        val openingCurly = '{'
+        val closingCurly = '}'
+        val openingSquare = '['
+        val closingSquare = ']'
+        val openingE = '('
+        val closingE = ')'
+
+        var result: Stack<Char> = Stack()
+
+        s.forEach {
+            when (it) {
+                openingCurly -> result.push(openingCurly)
+                openingE -> result.push(openingE)
+                openingSquare -> result.push(openingSquare)
+                closingE ->  {if (result.peek().equals(openingE)) result.pop() else return false}
+                closingSquare-> {if (result.peek().equals(openingSquare)) result.pop() else return false}
+                closingCurly ->  {if (result.peek().equals(openingCurly)) result.pop() else return false}
+            }
+        }
+
+        return result.isEmpty()
+    }
+}
+
+
