19f961cce321877b1e58f213e6330b7b95086be2
nikhiljoshi <nikhiljoshi08@gmail.com>
2021-09-20 17:42:44 +0100

    Longest repeating

---
+++ main/kotlin/Numbers/Multiplication.kt
@@ -0,0 +1,19 @@
+package Numbers
+//Reverse seqauence
+fun main() {
+
+
+    println(KataSolution.multiplicationTable(3))
+
+
+}
+
+object KataSolution {
+    fun multiplicationTable(size: Int) {
+
+        var result:IntArray
+
+        var resultTemp = listOf<Int>()
+
+    }
+}
---
+++ main/kotlin/String/CountDupslicates.kt
@@ -0,0 +1,26 @@
+package String
+
+import java.util.Collections.max
+
+fun longestChar(input:String):Char {
+    val result =input.groupingBy {it} .eachCount ()
+
+    println(max(result.values))
+
+    println(getKey(result, max(result.values)))
+
+    return getKey(result, max(result.values))
+
+}
+
+fun <K, V> getKey(map: Map<K, V>, target: V): K {
+    return map.keys.first { target == map[it] };
+}
+
+
+fun main()
+{
+    val sampleString = "aaabbbbbccc"
+    println("Longest repeating is :" + longestChar(sampleString))
+    println("Longest repeating is :" +longestChar("aabbcccc"))
+}
